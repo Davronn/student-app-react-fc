5be18ee3c1853093e7f8d04549cf152fc78cbffb
Davronn <davronmahamadjonov8@gmai.com>
2024-04-03 17:05:12 +0500

    map teachers

--- src/components/ProfileLinkTo.jsx
+++ src/components/ProfileLinkTo.jsx
@@ -1,16 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "../pages/Login";
 import ToLogin from "./ToLogin";
 import ProfileInfo from "./ProfileLink";
 
 function Profile() {
   const username = localStorage.getItem("userName");
+  
   console.log(username);
-  return (
-    <div>
-       {username ? "" : <ToLogin/>}
-    </div>
-  )
-} 
+  return <div>{username ? "" : <ToLogin />}</div>;
+}
 
 export default Profile;
--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import Profile from './ProfileLinkTo'
+import React, { useEffect, useState } from "react";
+import Profile from "./ProfileLinkTo";
+import axios from "axios";
+import UniversalButton from "./StudentListStyled";
 
 function Teachers() {
+  const [teachers, setTeachers] = useState([]);
+  console.log(teachers);
+  useEffect(() => {
+    const fetchTeahcer = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/teachers");
+        setTeachers(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+    fetchTeahcer();
+  }, []);
+
+
   return (
-    <div className='container'>
-      <div className="d-flex justify-content-between align-items-center p-2" >
+    <div className="container">
+      <div className="d-flex justify-content-between align-items-center p-2">
         <h2 className="my-3">Teachers app</h2>
         <div className="">
           <Profile />
@@ -17,8 +34,43 @@ function Teachers() {
           placeholder="Search by First Name , Last Name and Group"
         />
       </div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Group</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teachers.map((student, i) => (
+            <tr key={student.id}>
+              <th scope="row">{i + 1}</th>
+              <td>{student.firstName}</td>
+              <td>{student.lastName}</td>
+              <td>{student.group}</td>
+              <td>
+                <UniversalButton
+                  className="mx-1"
+                  onClick={() => openEditModal(student)}
+                >
+                  Edit
+                </UniversalButton>
+                <UniversalButton
+                  variant="delete"
+                  onClick={() => handleDelete(student.id)}
+                >
+                  Delete
+                </UniversalButton>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Teachers
+export default Teachers;
